c96445373774c4758bf11e775c8da3028f60fa54
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-01-31 12:54:39 +0100

    Add files via upload

---
+++ WeBrowser/WeBrowser/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace WeBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btNatrag = new System.Windows.Forms.Button();
+            this.btNaprijed = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.btnIdi = new System.Windows.Forms.Button();
+            this.btnWebmail = new System.Windows.Forms.Button();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnGmail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btNatrag
+            // 
+            this.btNatrag.Location = new System.Drawing.Point(12, 12);
+            this.btNatrag.Name = "btNatrag";
+            this.btNatrag.Size = new System.Drawing.Size(75, 23);
+            this.btNatrag.TabIndex = 0;
+            this.btNatrag.Text = "Natrag";
+            this.btNatrag.UseVisualStyleBackColor = true;
+            this.btNatrag.Click += new System.EventHandler(this.btNatrag_Click);
+            // 
+            // btNaprijed
+            // 
+            this.btNaprijed.Location = new System.Drawing.Point(109, 12);
+            this.btNaprijed.Name = "btNaprijed";
+            this.btNaprijed.Size = new System.Drawing.Size(75, 23);
+            this.btNaprijed.TabIndex = 1;
+            this.btNaprijed.Text = "Naprijed";
+            this.btNaprijed.UseVisualStyleBackColor = true;
+            this.btNaprijed.Click += new System.EventHandler(this.btNaprijed_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adresa:";
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Location = new System.Drawing.Point(258, 17);
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(222, 20);
+            this.txtAdresa.TabIndex = 3;
+            this.txtAdresa.TextChanged += new System.EventHandler(this.txtAdresa_TextChanged);
+            // 
+            // btnIdi
+            // 
+            this.btnIdi.Location = new System.Drawing.Point(499, 15);
+            this.btnIdi.Name = "btnIdi";
+            this.btnIdi.Size = new System.Drawing.Size(44, 23);
+            this.btnIdi.TabIndex = 4;
+            this.btnIdi.Text = "Idi";
+            this.btnIdi.UseVisualStyleBackColor = true;
+            this.btnIdi.Click += new System.EventHandler(this.btnIdi_Click);
+            // 
+            // btnWebmail
+            // 
+            this.btnWebmail.Location = new System.Drawing.Point(726, 12);
+            this.btnWebmail.Name = "btnWebmail";
+            this.btnWebmail.Size = new System.Drawing.Size(76, 23);
+            this.btnWebmail.TabIndex = 5;
+            this.btnWebmail.Text = "Webmail";
+            this.btnWebmail.UseVisualStyleBackColor = true;
+            this.btnWebmail.Click += new System.EventHandler(this.btnWebmail_Click);
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(825, 12);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(44, 23);
+            this.btnIspis.TabIndex = 6;
+            this.btnIspis.Text = "Ispis";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(13, 42);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1027, 581);
+            this.webBrowser1.TabIndex = 7;
+            this.webBrowser1.Url = new System.Uri("http://web2.ss-zcrnje-rovinj.skole.hr/", System.UriKind.Absolute);
+            // 
+            // btnGmail
+            // 
+            this.btnGmail.Location = new System.Drawing.Point(628, 12);
+            this.btnGmail.Name = "btnGmail";
+            this.btnGmail.Size = new System.Drawing.Size(75, 23);
+            this.btnGmail.TabIndex = 8;
+            this.btnGmail.Text = "Gmail";
+            this.btnGmail.UseVisualStyleBackColor = true;
+            this.btnGmail.Click += new System.EventHandler(this.btnGmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1038, 625);
+            this.Controls.Add(this.btnGmail);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.btnWebmail);
+            this.Controls.Add(this.btnIdi);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btNaprijed);
+            this.Controls.Add(this.btNatrag);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shcum Browser™";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btNatrag;
+        private System.Windows.Forms.Button btNaprijed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.Button btnIdi;
+        private System.Windows.Forms.Button btnWebmail;
+        private System.Windows.Forms.Button btnIspis;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnGmail;
+    }
+}
+
---
+++ WeBrowser/WeBrowser/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btNatrag_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void webBrowser1_CamGoBackChanged(object sender, EventArgs e) 
+        {
+            btNatrag.Enabled = webBrowser1.CanGoBack;
+        }
+
+        private void btNaprijed_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+        
+        private void webBrowser1_CamGoFowardChanged(object sender, EventArgs e)
+        {
+            btNaprijed.Enabled = webBrowser1.CanGoForward;
+        }
+
+        private void btnIdi_Click(object sender, EventArgs e)
+        {
+
+            webBrowser1.Navigate(txtAdresa.Text);
+        }
+
+        private void txtAdresa_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowPrintDialog();
+        }
+
+        private void btnWebmail_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate("https://webmail.carnet.hr/login.php");
+        }
+
+        private void btnGmail_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate("https://mail.google.com/");
+        }
+        
+        private void Navigate(string adresa)
+        {
+            if (String.IsNullOrEmpty(adresa)) return;
+            if (adresa.Equals("about:blank")) return;
+            if (!adresa.StartsWith("https://") && !adresa.StartsWith("http://"))
+            {
+                adresa = "http://" + adresa;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(adresa));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+
+        private void txtAdresa_keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(txtAdresa.Text);
+            }
+        }
+    }
+}
